5408fb5028ead56aadfb0845a1d1e92990afb992
Dane Walton <dane.walton@microsoft.com>
2020-05-27 15:07:56 -0700

    Add some comments

--- sdk/iot/hub/samples/src/sample_base64.h
+++ sdk/iot/hub/samples/src/sample_base64.h
@@ -4,8 +4,11 @@
 #include "az_result.h"
 #include "az_span.h"
 
+// Decode an input span from base64 to bytes
 az_result sample_base64_decode(az_span base64_encoded, az_span in_span, az_span* out_span);
 
+// Encode an input span to base64
 az_result sample_base64_encode(az_span bytes, az_span in_span, az_span* out_span);
 
+// HMAC256 an input span with an input key
 az_result sample_hmac_encrypt(az_span key, az_span bytes, az_span in_span, az_span* out_span);
